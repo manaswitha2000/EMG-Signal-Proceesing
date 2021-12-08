0aa154ffeadea575eb3ea96454212da7348d7f4c
manaswitha2000 <56600071+manaswitha2000@users.noreply.github.com>
2021-12-08 11:53:12 +0530

    Add files via upload

---
+++ binary.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("ElbowandWrist.csv")
+
+from sklearn.model_selection import train_test_split
+training_set, test_set = train_test_split(data, test_size = 0.2, random_state = 1)
+X_train = training_set.iloc[:,0:2].values
+Y_train = training_set.iloc[:,2].values
+print(X_train)
+print("******")
+print(Y_train)
+X_test = test_set.iloc[:,0:2].values
+Y_test = test_set.iloc[:,2].values
+print("******")
+print(X_test)
+print("******")
+print(Y_test)
+
+# Feature Scaling
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+# Fitting the classifier into the Training set
+
+from sklearn.svm import SVC
+classifier = SVC(kernel='rbf', random_state = 1)
+classifier.fit(X_train,Y_train)
+
+# Prdicting the test set results
+
+Y_pred = classifier.predict(X_test)
+#est_set["Predictions"] = Y_pred
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test,Y_pred)
+
+accuracy = float(cm.diagonal().sum())/len(Y_test)
+print("\nAccuracy Of SVM For The Given Dataset : ", accuracy)
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, Y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('black', 'white')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'orange'))(i), label = j)
+plt.title('Elbow = 0 and Wrist = 1')
+plt.xlabel('EMG reading')
+plt.ylabel('EMG reading')
+plt.legend()
+plt.show()
